cb72228f40de8e6f2e3d8b1ee5506c44aefedb4c
soraew <soraew@gmail.com>
2020-11-03 23:00:14 +0900

    initial commit

---
+++ 2020_7/beta.py
@@ -0,0 +1,21 @@
+import numpy as np
+import scipy.stats as st
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+import sys
+
+print(sys.platform)
+
+FontPath = '/System/Library/Fonts/ヒラギノ角ゴシック W4.ttc'
+
+jpfont = FontProperties(fname=FontPath)
+    
+fig1 = plt.figure(num=1, facecolor='w')
+q = np.linspace(0, 1, 250)
+plt.plot(q, st.uniform.pdf(q), 'k')
+plt.plot(q, st.beta.pdf(q, 0.5, 0.5), 'c--')
+plt.xlim(0, 1)
+plt.ylim(0, 2.8)
+plt.legend(['一様分布', 'ベータ分布'], prop=jpfont)
+plt.savefig('uni_beta.png', dpi=300)
+plt.show()
